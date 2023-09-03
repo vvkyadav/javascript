53313d8e0368bf3c62c074a035655be8978d581e
Vivek Yadav <88069541+vvkyadav@users.noreply.github.com>
2023-09-03 17:13:00 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,32 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Hitesh")
+
+
+let name = "vivek"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// Premitive Data Type
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+console.log(typeof NaN); // number
