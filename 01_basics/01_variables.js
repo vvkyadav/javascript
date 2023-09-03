57ee4f366e7a1af790addc240307018ba21913d3
Vivek Yadav <88069541+vvkyadav@users.noreply.github.com>
2023-09-03 14:42:38 +0000

    variable

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 12345;
+let accountEmail = "vyadav166@gmail.com";
+var accountPassword = "12244";
+accountCity = "Noida";
+
+// accountId = 4; // not allowed
+
+console.log(accountId);
