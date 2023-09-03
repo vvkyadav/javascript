21f21f8f2040b8fa0b2babf1e604cbef40408179
Vivek Yadav <88069541+vvkyadav@users.noreply.github.com>
2023-09-03 17:41:50 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,34 @@
+// let score = "33avvv"; //string      // number NaN
+// let score = "null";   //string       // number NaN
+// let score = undefined; //undefined   // number NaN
+// let score = true;  //boolean         // number 1
+// let score = "vivek";  //string       //number NaN
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "hitesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
