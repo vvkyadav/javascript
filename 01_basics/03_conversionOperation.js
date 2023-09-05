7581e44104250caa08f7c5b58897c1ceb1b8ad9e
Vivek Yadav <88069541+vvkyadav@users.noreply.github.com>
2023-09-05 17:45:43 +0000

    operators

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -7,7 +7,7 @@
 // console.log(typeof score);
 // console.log(typeof(score));
 
-let valueInNumber = Number(score)
+//let valueInNumber = Number(score)
 //console.log(typeof valueInNumber);
 //console.log(valueInNumber);
 
@@ -32,3 +32,42 @@ let stringNumber = String(someNumber)
 // console.log(typeof stringNumber);
 
 
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2); //4
+// console.log(2-2); //0
+// console.log(2*2); //4
+// console.log(2**3); //8
+// console.log(2/3); //0.6666
+// console.log(2%3); //2
+
+let str1 = "hello"
+let str2 = " vivek"
+
+let str3 = str1 + str2
+// console.log(str3); //hello vivek
+
+// console.log("1" + 2); //12
+// console.log(1 + "2"); //12
+// console.log("1" + 2 + 2); //122
+// console.log(1 + 2 + "2"); //32
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+// console.log(gameCounter); //101
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
